8696b5ba345edec9b573624a524e7b96aa689aea
Leo Villalobos <villaleobos@gmail.com>
2023-02-02 13:47:20 -0800

    Create detailedView segue screen

--- DinoGuesser/DetailViewController.swift
+++ DinoGuesser/DetailViewController.swift
@@ -11,13 +11,31 @@ var detailViewCtrlSegueName: String = "detailedView"
 
 class DetailViewController: UIViewController {
     var dinosaur: Dinosaur?
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var weightKg: UILabel!
+    @IBOutlet weak var heightMeters: UILabel!
+    @IBOutlet weak var diet: UILabel!
+    @IBOutlet weak var era: UILabel!
+    @IBOutlet weak var region: UILabel!
+    @IBOutlet weak var speedMph: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // NOTE: Debugging
-        if let someDinosaur: Dinosaur = dinosaur {
-            print(someDinosaur.name)
+        if let some: Dinosaur = dinosaur {
+            updateText(&name, to: some.name)
+            imageView.image = some.image
+            updateText(&type, to: some.type)
+            updateText(&weightKg, to: String(some.weight))
+            updateText(&heightMeters, to: String(some.height))
+            updateText(&diet, to: some.diet)
+            updateText(&era, to: some.era)
+            updateText(&region, to: some.region)
+            updateText(&speedMph, to: String(some.speed))
         }
     }
+    
+    internal func updateText(_ label: inout UILabel, to: String) { label.text = to }
 }
